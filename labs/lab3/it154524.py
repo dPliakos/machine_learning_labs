6323844c63f2c92a6fb760021533f42d49386b12
dpliakos <dimitrispl96@gmail.com>
2019-04-03 17:49:42 +0300

    Add lab3

---
+++ labs/lab3/it154524.py
@@ -0,0 +1,22 @@
+"""Notes."""
+
+import numpy as np
+
+w = np.random.randn(5)  # Gaussian
+
+# ston tipo prepei na einai h p  grammi oxi i stiliself.
+
+xtrain = np.random.randn(135, 5)  # paradeigma
+
+p = 25
+
+# xtrain = epaujimenos
+xtrain([p, :].dot(w))
+
+if u > 0:
+    y = 1
+else:
+    y = 0
+
+# magkia
+y = (u > 0).astype(float)
